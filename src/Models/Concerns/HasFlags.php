5a2a79c318b8c87490417c68e5782ddba4418213
freekmurze <freekmurze@users.noreply.github.com>
2022-11-09 07:10:31 +0000

    Fix styling

--- src/Models/Concerns/HasFlags.php
+++ src/Models/Concerns/HasFlags.php
@@ -3,9 +3,9 @@
 namespace Spatie\ModelFlags\Models\Concerns;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Spatie\ModelFlags\Models\Flag;
-use Illuminate\Database\Eloquent\Model;
 
 /** @mixin Model */
 trait HasFlags
